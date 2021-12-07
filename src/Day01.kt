653b8fe83f254a1d8f981d4300866d91c538f59f
tobias.grunwald <tobias.grunwald@lise.de>
2021-12-07 15:26:20 +0100

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,28 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int = countIncreases(input.map(String::toInt))
+    fun part2(input: List<String>): Int = countIncreases(buildWindows(input.map(String::toInt)))
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(testInput))
+    println(part2(testInput))
+
+    println("-----")
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun countIncreases(input: List<Int>) = input.filterIndexed { i, num ->
+    if (i == 0) false
+    else num > input[i - 1]
+}.count()
+
+fun buildWindows(input: List<Int>): List<Int> {
+    return input.mapIndexedNotNull { i, num ->
+        if (i == 0 || i == 1) null
+        else num + input[i - 1] + input[i - 2]
+    }
 }
